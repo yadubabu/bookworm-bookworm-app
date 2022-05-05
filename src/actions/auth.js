c17800a58612a5788f876f9574989893ebd11041
yadubabu <100501154+yadubabu@users.noreply.github.com>
2022-05-05 10:52:04 +0530

    Update auth.js

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import {USER_LOGGED_IN} from '../types';
+import {USER_LOGGED_IN,USER_LOGGED_OUT} from '../types';
 import api from '../api';
 
 
@@ -9,5 +9,17 @@ export const userLoggedIn=user=>({
 });
 
 export const login=credentials=>dispatch=>
- api.user.login(credentials).then(user=>dispatch(userLoggedIn(user)));
+ api.user.login(credentials).then(user=>{
+    localStorage.bookwormJWT=user.email.token; 
+    dispatch(userLoggedIn(user));
+    });
 
+    export const userLoggedOut=()=>({
+        type:USER_LOGGED_OUT
+        
+    });
+
+    export const logout=()=>dispatch=>{
+       localStorage.removeItem('bookwormJWT'); 
+       dispatch(userLoggedOut());
+       };
