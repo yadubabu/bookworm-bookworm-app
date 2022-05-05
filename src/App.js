ba11a7cdf278cae60ba291a6830fb20d0132ce9b
yadubabu <100501154+yadubabu@users.noreply.github.com>
2022-05-05 22:16:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from "react";
-import {Routes,Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
+import DashboardPage from './components/pages/DashboardPage';
+import SignupPage from './components/pages/SignupPage';
+
+import UserRoute from './components/routes/UserRoute';
+import GuestRoute from './components/routes/GuestRoute';
+
+import PropTypes from 'prop-types';
 
 
-const App=()=>
+const App=({location})=>(
   <div className="ui container">
-    <Routes>
-      <Route path="/" element={<HomePage />}/>
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    
+      <Route location={location} path="/" exact component={HomePage }/>
+      <GuestRoute location={location} path="/login" exact component={LoginPage } /> 
+      <GuestRoute location={location} path="/signup" exact component={SignupPage } /> 
 
-  </div>
+      <UserRoute location={location} path="/dashboard" exact component={DashboardPage } />
+
+    
+
+  </div>);
+
+  App.prototypes={
+    lacation:PropTypes.shape({
+      pathname:PropTypes.string.isRequired
+    }).isRequired
+  };
 
 
 export default App;
