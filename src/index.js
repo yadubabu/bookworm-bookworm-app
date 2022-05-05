93c6ac31515601df979ca89e24103c62b5d4e6cd
yadubabu <100501154+yadubabu@users.noreply.github.com>
2022-05-05 22:18:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter,Route} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore,applyMiddleware} from 'redux';
@@ -8,14 +8,20 @@ import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { userLoggedIn } from './actions/auth';
 
 const store=createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk))
 );
 
+if(localStorage.bookwormJWT){
+  const user={token:localStorage.bookwormJWT};
+  store.dispatch(userLoggedIn(user));
+}
+
 ReactDOM.render(
     <BrowserRouter>
       <Provider store={store}>
-        <App/>
+        <Route component={App}/>
       </Provider>
 
  </BrowserRouter>,document.getElementById('root')
