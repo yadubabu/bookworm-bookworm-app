4b6bd170d8bba07f2980c7a5784497446d6229b4
yadubabu <100501154+yadubabu@users.noreply.github.com>
2022-05-05 22:12:18 +0530

    Add files via upload

---
+++ src/components/messages/ConfirmEmailPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { MessageHeader } from "semantic-ui-react";
+import { Message } from "semantic-ui-react";
+
+const ConfirmEmailMessage=()=>(
+    <Message info>
+        <MessageHeader>
+            Please,Verify your email to unlock awesomeness
+        </MessageHeader>
+    </Message>
+
+);
+
+export default ConfirmEmailMessage;
+
+
