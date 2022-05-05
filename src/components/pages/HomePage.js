72716a70f4cc5db93ebcbe5ec34cf5e457d6fd61
yadubabu <100501154+yadubabu@users.noreply.github.com>
2022-05-05 10:52:55 +0530

    Update HomePage.js

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import  * as actions from '../../actions/auth';
 
-const HomePage = () => {
-  return (
-    <div><h1>HomePage</h1>
-    <Link to="/login">Login</Link>
+const HomePage = ({ isAuthenticated , logout}) => 
+  (
+    <div>
+      <h1>Home Page</h1>
+      {isAuthenticated ? <button onClick={()=>logout()}>Logout</button> : <Link to="/login">Login</Link>}
     </div>
-  )
+  );
+HomePage.prototypes={
+  isAuthenticated:PropTypes.bool.isRequired,
+  logout:PropTypes.func.isRequired
+};
+
+function mapStateToProps(state){
+  return {
+    isAuthenticated:!!state.user.token
+  }
 }
 
-export default HomePage;
+export default connect(mapStateToProps,{logout:actions.logout})(HomePage);
