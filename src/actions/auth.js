8e024036a51cef1a9eca1f793048d205649bbe72
yadubabu <100501154+yadubabu@users.noreply.github.com>
2022-05-06 21:57:08 +0530

    Update auth.js

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -23,3 +23,17 @@ export const login=credentials=>dispatch=>
        localStorage.removeItem('bookwormJWT'); 
        dispatch(userLoggedOut());
        };
+
+    export const confirm =(token)=>(dispatch)=>api.user.confirm(token)
+    .then(user=>{
+        localStorage.bookwormJWT=user.token;
+        dispatch(userLoggedIn(user));
+    });
+
+    export const resetPasswordRequest=({email})=>()=>
+        api.user.resetPasswordRequest(email);
+
+    export const validateToken=(token)=>()=>
+        api.user.validateToken(token);
+
+    export const resetPassword=(data)=>()=>api.user.resetPassword(data);
